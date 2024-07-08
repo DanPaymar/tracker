bac4d96d6d1df2120760b5ca1ef9ad75bf603ca2
arcticchimpanzee <152743640+arcticchimpanzee@users.noreply.github.com>
2024-07-08 16:35:05 -0700

    update new trip destination to use mapkit and save location.

---
+++ tracker/ViewModel/AddNewTripViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AddNewTripViewModel.swift
+//  tracker
+//
+//  Created by Daniel Paymar on 7/8/24.
+//
+
+import Foundation
---
+++ tracker/ViewModel/PlaceVM.swift
@@ -0,0 +1,8 @@
+//
+//  PlaceVM.swift
+//  tracker
+//
+//  Created by Daniel Paymar on 7/5/24.
+//
+
+import Foundation
